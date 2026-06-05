5ba59867398e14a774bb7282dfc3eea9d51827f4
Kary <karylopez1720@gmail.com>
2026-06-05 19:32:50 -0300

    mover claves a env

--- firebase.js
+++ firebase.js
@@ -3,12 +3,12 @@ import { getFirestore } from "firebase/firestore";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCp8c93NuVHJ7k7WQ-hUMqCEwnBwfsWYG8",
-  authDomain: "studyflow-9d48c.firebaseapp.com",
-  projectId: "studyflow-9d48c",
-  storageBucket: "studyflow-9d48c.firebasestorage.app",
-  messagingSenderId: "931245611410",
-  appId: "1:931245611410:web:d0b190325f95664043094b"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID
 };
 
 const app = initializeApp(firebaseConfig);
